eab70fff1fe29a4419d7563b57f90605a0399bf4
AdielGG <adiel@uci.cu>
2025-03-10 14:31:11 -0400

    Instalando Vuetify y Vite

---
+++ Frontend/tienda_oasis/src/components/TheWelcome.vue
@@ -0,0 +1,21 @@
+<template>
+  <header>
+    <h1>
+      Tienda Oasis
+    </h1>
+    
+  </header>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style>
+
+</style>
--- Frontend/tienda_oasis/src/main.js
+++ Frontend/tienda_oasis/src/main.js
@@ -1,6 +1,14 @@
+import './assets/main.css'
+
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import store from './store'
+import vuetify from './plugins/vuetify'
 
-createApp(App).use(store).use(router).mount('#app')
+const app = createApp(App)
+
+app.use(router)
+
+app.use(vuetify)
+
+app.mount('#app')
