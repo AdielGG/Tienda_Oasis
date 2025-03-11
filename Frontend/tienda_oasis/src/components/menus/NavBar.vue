d0770650b6517f34f5f8cd8289072fdece201090
AdielGG <adiel@uci.cu>
2025-03-10 23:19:08 -0400

    Pagina Principal Creada

---
+++ Frontend/tienda_oasis/src/components/menus/NavBar.vue
@@ -0,0 +1,65 @@
+<template>
+    <nav>
+        <ul class="nav-bar">
+            <li>
+                <a href="" class="nav-item">Inicio</a>
+            </li>
+            <li>
+                <a href="" class="nav-item">Productos</a>
+            </li>
+            <li>
+                <a href="" class="nav-item">Nosotros</a>
+            </li>
+            <li>
+                <a href="" class="nav-item">Ayuda</a>
+            </li>
+            <v-spacer></v-spacer>
+            <v-btn class="nav-btn second">
+                Iniciar Sesion
+            </v-btn>
+            <v-btn class="nav-btn primary">
+                Registrarse
+            </v-btn>
+        </ul>
+    </nav>
+</template>
+
+<script>
+
+</script>
+
+<style scoped>
+
+.nav-bar{
+    border-radius: 10px;
+    display: flex;
+    background-color: rgb(187, 35, 35);
+}
+
+li {
+    margin: 10px;
+    list-style: none;
+}
+
+.nav-item{
+    color: black;
+    font-size: 2em;
+    font-family:Georgia, 'Times New Roman', Times, serif;
+}
+
+.nav-item:hover{
+    background-color: rgba(0, 0, 0, 0);
+    border-bottom: solid 1px black;
+}
+.nav-btn{
+    width: auto;
+    margin-top: 1em;
+    margin-right: 1em;
+}
+
+.primary{
+    background-color: rgb(238, 255, 0);
+    color: rgb(0, 0, 0);
+}
+
+</style>
--- Frontend/tienda_oasis/src/views/HomeView.vue
+++ Frontend/tienda_oasis/src/views/HomeView.vue
@@ -1,9 +1,52 @@
 <script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+import NavBar from '@/components/menus/NavBar.vue';
 </script>
 
 <template>
   <main>
-    <TheWelcome />
+    <video src="../assets/video.mp4" autoplay="true" muted="true" loop="true" ></video>
+    
+    <header class="header">
+
+      <h1> <b>Tienda Oasis</b> </h1>
+    </header>
+    
+    <NavBar/>
+
+
   </main>
 </template>
+
+
+<style scoped>
+ 
+  .header{
+    background-color: rgba(13, 20, 20, 0);
+    width: 100%;
+    text-align: center;
+    
+
+  }
+  .header h1{
+    
+    color: rgb(255, 238, 0);
+    font-size: 4em;
+    font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+  }
+
+  main{
+    width: 100%;
+    height: 100%;
+    display: block;
+  }
+  
+  video {
+  position: fixed;
+  right: 0;
+  bottom: 0;
+  min-width: 105%;
+  min-height: 100%;
+  transform: translateX(calc((100% - 100vw) / 2));
+  z-index: -2;
+}
+</style>
